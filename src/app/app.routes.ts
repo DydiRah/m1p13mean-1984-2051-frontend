ad7e043bb0b879350d6f7e785e403b28c549cabf
DydiRah <rahelisonDylan@gmail.com>
2026-03-01 13:09:07 +0300

    Setting up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,11 @@
 import { Routes } from "@angular/router";
 import { EcommerceComponent } from "./pages/dashboard/ecommerce/ecommerce.component";
 import { ProfileComponent } from "./pages/profile/profile.component";
-import { FormElementsComponent } from "./pages/forms/form-elements/form-elements.component";
-import { BasicTablesComponent } from "./pages/tables/basic-tables/basic-tables.component";
-import { BlankComponent } from "./pages/blank/blank.component";
 import { NotFoundComponent } from "./pages/other-page/not-found/not-found.component";
 import { AppLayoutComponent } from "./shared/layout/app-layout/app-layout.component";
 import { InvoicesComponent } from "./pages/invoices/invoices.component";
-import { LineChartComponent } from "./pages/charts/line-chart/line-chart.component";
-import { BarChartComponent } from "./pages/charts/bar-chart/bar-chart.component";
-import { AlertsComponent } from "./pages/ui-elements/alerts/alerts.component";
-import { AvatarElementComponent } from "./pages/ui-elements/avatar-element/avatar-element.component";
-import { BadgesComponent } from "./pages/ui-elements/badges/badges.component";
-import { ButtonsComponent } from "./pages/ui-elements/buttons/buttons.component";
-import { ImagesComponent } from "./pages/ui-elements/images/images.component";
-import { VideosComponent } from "./pages/ui-elements/videos/videos.component";
 import { SignInComponent } from "./pages/auth-pages/sign-in/sign-in.component";
 import { SignUpComponent } from "./pages/auth-pages/sign-up/sign-up.component";
-import { CalenderComponent } from "./pages/calender/calender.component";
 import { BoxesComponent } from "./pages/boxes/boxes.component";
 import { StoresComponent } from "./pages/stores/stores.component";
 import { StockesComponent } from "./pages/stockes/stockes.component";
@@ -34,66 +22,60 @@ export const routes: Routes = [
     canActivateChild: [roleGuard],
     children: [
       {
-        path: "",
+        path: "dashboard",
         component: EcommerceComponent,
         pathMatch: "full",
         title:
-          "Angular Ecommerce Dashboard | TailAdmin - Angular Admin Dashboard Template",
+          "Dashboard - Vendeo",
         data: { roles: ['admin', 'store'] }
       },
       {
         path: "stores",
         component: StoresComponent,
         title:
-          "Angular Boxes | TailAdmin - Angular Admin Dashboard Template",
+          "Stores - Vendeo",
         data: { roles: ['admin', 'store'] }
       },
       {
         path: "contract/:boxId",
         component: ContractComponent,
-        title: "Angular Boxes | TailAdmin - Angular Admin Dashboard Template",
+        title: "Contract - Vendeo",
         data: { roles: ['admin'] }
       },
       {
         path: "boxes",
         component: BoxesComponent,
         title:
-          "Angular Boxes | TailAdmin - Angular Admin Dashboard Template",
+          "Boxes - Vendeo",
         data: { roles: ['admin', 'store'] }
       },
       {
         path: "stockes",
         component: StockesComponent,
         title:
-          "Angular Stockes | TailAdmin - Angular Admin Dashboard Template",
+          "Stockes - Vendeo",
         data: { roles: ['store'] }
       },
       {
         path: "items",
         component: ItemsComponent,
         title:
-          "Angular Items | TailAdmin - Angular Admin Dashboard Template",
+          "Items - Vendeo",
         data: { roles: ['store', 'buyer'] }
       },
       {
         path: "orders",
         component: OrdersComponent,
         title:
-          "Angular Orders | TailAdmin - Angular Admin Dashboard Template",
+          "Orders - Vendeo",
         data: { roles: ['store', 'buyer'] }
       },
       {
         path: "all-items",
         component: ItemsBuyerComponent,
         title:
-          "Items",
+          "Items - Vendeo",
         data: { roles: ['buyer'] }
-      },
-      {
-        path: "calendar",
-        component: CalenderComponent,
-        title:
-          "Angular Calender | TailAdmin - Angular Admin Dashboard Template",
       },
       {
         path: "profile",
@@ -103,77 +85,10 @@ export const routes: Routes = [
         data: { roles: ['buyer', 'admin', 'store'] }
       },
       {
-        path: "form-elements",
-        component: FormElementsComponent,
-        title:
-          "Angular Form Elements Dashboard | TailAdmin - Angular Admin Dashboard Template",
-      },
-      {
-        path: "basic-tables",
-        component: BasicTablesComponent,
-        title:
-          "Angular Basic Tables Dashboard | TailAdmin - Angular Admin Dashboard Template",
-      },
-      {
-        path: "blank",
-        component: BlankComponent,
-        title:
-          "Angular Blank Dashboard | TailAdmin - Angular Admin Dashboard Template",
-      },
-      // support tickets
-      {
         path: "invoice",
         component: InvoicesComponent,
         title:
-          "Angular Invoice Details Dashboard | TailAdmin - Angular Admin Dashboard Template",
-      },
-      {
-        path: "line-chart",
-        component: LineChartComponent,
-        title:
-          "Angular Line Chart Dashboard | TailAdmin - Angular Admin Dashboard Template",
-      },
-      {
-        path: "bar-chart",
-        component: BarChartComponent,
-        title:
-          "Angular Bar Chart Dashboard | TailAdmin - Angular Admin Dashboard Template",
-      },
-      {
-        path: "alerts",
-        component: AlertsComponent,
-        title:
-          "Angular Alerts Dashboard | TailAdmin - Angular Admin Dashboard Template",
-      },
-      {
-        path: "avatars",
-        component: AvatarElementComponent,
-        title:
-          "Angular Avatars Dashboard | TailAdmin - Angular Admin Dashboard Template",
-      },
-      {
-        path: "badge",
-        component: BadgesComponent,
-        title:
-          "Angular Badges Dashboard | TailAdmin - Angular Admin Dashboard Template",
-      },
-      {
-        path: "buttons",
-        component: ButtonsComponent,
-        title:
-          "Angular Buttons Dashboard | TailAdmin - Angular Admin Dashboard Template",
-      },
-      {
-        path: "images",
-        component: ImagesComponent,
-        title:
-          "Angular Images Dashboard | TailAdmin - Angular Admin Dashboard Template",
-      },
-      {
-        path: "videos",
-        component: VideosComponent,
-        title:
-          "Angular Videos Dashboard | TailAdmin - Angular Admin Dashboard Template",
+          "Invoice - Vendeo",
       },
     ],
   },
